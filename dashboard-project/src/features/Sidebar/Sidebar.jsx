cf3509ee42ff251ef07e3e2c1852ce2cbf0e5eba
Alireza <andromedaa5692@gmail.com>
2026-09-08 19:16:06 +0330

    comleted Sidebar feature with two components -- SidebarHeader, Menus(dynamic)

--- dashboard-project/src/features/Sidebar/Sidebar.jsx
+++ dashboard-project/src/features/Sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SidebarHeader from "./components/SidebarHeader";
+import menus from "../../data/menus";
+import Menus from "./components/Menus";
 
 const Sidebar = () => {
   return (
     <aside className=" p-6 w-[272px] relative z-10 bg-white h-screen sticky top-0 border-l primary-border-color">
       <SidebarHeader />
-      {/* <SidebarMenus /> */}
+      <Menus menus={menus} />
     </aside>
   );
 };
