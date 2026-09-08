c3af53e2d8a13fc7c0fe7608eaac50c061634947
Alireza <andromedaa5692@gmail.com>
2026-09-08 16:00:21 +0330

    add css, fonts, images to public folder in project and import css files in main.jsx

--- dashboard-project/src/main.jsx
+++ dashboard-project/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+import "../public/css/index.css";
+import "../public/css/fonts.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
