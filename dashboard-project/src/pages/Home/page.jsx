147f10f5ff14bfffeb270fc15fe328a5790e5f10
Alireza <andromedaa5692@gmail.com>
2026-09-09 15:03:13 +0330

    develop Home page :: SectionTitle Component

--- dashboard-project/src/pages/Home/page.jsx
+++ dashboard-project/src/pages/Home/page.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import SectionTitle from "../../components/common/SectionTitle";
+import { useNavigate } from "react-router";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [isRedirecting, setIsRedirecting] = useState(false);
+  const navigate = useNavigate();
+
+  const toggle = () => {
+    setIsRedirecting(!isRedirecting);
+  };
+
+  const CTAButton = () => {
+    const clickHandler = () => {
+      toggle();
+
+      setTimeout(() => {
+        navigate("/products");
+      }, 2000);
+    };
+
+    return (
+      <button
+        onClick={clickHandler}
+        className="primary-bg px-4 py-2 text-sm rounded-md cursor-pointer hover:opacity-90 text-white"
+      >
+        {isRedirecting ? "درحال انتقال ..." : "ایجاد محصول"}
+      </button>
+    );
+  };
+  return (
+    <>
+      <SectionTitle title="داشبورد" Buttons={<CTAButton />} />
+      {/* <Summaries /> */}
+
+      <div className="">
+        {/* <DetailsCharts /> */}
+        {/* <ProductsTable /> */}
+        {/* <OuickOverview /> */}
+      </div>
+    </>
+  );
 };
 
 export default Home;
