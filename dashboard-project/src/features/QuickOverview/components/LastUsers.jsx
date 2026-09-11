de4852054b81cc8294e6a56fc3f0b9a8c9be4250
Alireza <andromedaa5692@gmail.com>
2026-09-11 19:50:14 +0330

    fix bug: add mt-5 to LastUsers Parent

--- dashboard-project/src/features/QuickOverview/components/LastUsers.jsx
+++ dashboard-project/src/features/QuickOverview/components/LastUsers.jsx
@@ -6,9 +6,9 @@ import UserCard from "./UserCard";
 
 const LastUsers = () => {
   return (
-    <div className="col-span-2 max-h-max">
+    <div className="col-span-2 max-h-max ">
       <SectionTitle title={"آخرین کاربران"} />
-      <div>
+      <div className="mt-5">
         {users.slice(-5).map((user) => (
           <UserCard key={user.id} {...user} />
         ))}
