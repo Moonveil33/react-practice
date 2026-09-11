76bd86f52a7519100798593acd41333ce63bf89a
Alireza <andromedaa5692@gmail.com>
2026-09-11 20:06:48 +0330

    Start develop Products Layout...

---
+++ dashboard-project/src/features/ProductsView/ProductsTableView.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProductsTableView = ({ products }) => {
+  return (
+    <div>
+      {/* <Table>
+        <TableHead>
+          <TableHeadCell></TableHeadCell>
+          <TableHeadCell></TableHeadCell>
+          <TableHeadCell></TableHeadCell>
+          <TableHeadCell></TableHeadCell>
+        </TableHead>
+
+        <TableBody>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableBody>
+      </Table> */}
+    </div>
+  );
+};
+
+export default ProductsTableView;
--- dashboard-project/src/pages/Products/page.jsx
+++ dashboard-project/src/pages/Products/page.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { CiGrid41, CiViewTable } from "react-icons/ci";
+import SectionTitle from "./../../components/common/SectionTitle";
+import { useState } from "react";
+import { products } from "./../../data/products";
+import ProductsTableView from "./../../features/ProductsView/ProductsTableView";
+import ProductsGridView from "./../../features/ProductsView/ProductsGridView";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [layoutType, setLayoutType] = useState("TABLE"); // or GRID
+
+  const toggleLayout = () => {
+    const layout = layoutType === "TABLE" ? "GRID" : "TABLE";
+    setLayoutType(layout);
+  };
+
+  const Buttons = (
+    <>
+      <button
+        onClick={toggleLayout}
+        className="text-2xl size-10 flex-center bg-[#ECEFF3] text-[#818898] *:stroke-1 rounded-md hover:bg-[#e1e4e7] active:scale-90 active:bg-[#ECEFF3]  duration-150 transition-all primary-border-color border cursor-pointer shadow"
+      >
+        {layoutType === "TABLE" ? <CiGrid41 /> : <CiViewTable />}
+      </button>
+
+      <button className="primary-bg px-3 py-1.5">ایجاد محصول</button>
+    </>
+  );
+
+  return (
+    <>
+      <SectionTitle title="لیست محصولات" Buttons={Buttons} />
+
+      <section className="mt-10 w-full! min-w-full!">
+        {layoutType === "TABLE" ? (
+          <ProductsTableView products={products} />
+        ) : (
+          <ProductsGridView products={products} />
+        )}
+      </section>
+    </>
+  );
 };
 
 export default Products;
