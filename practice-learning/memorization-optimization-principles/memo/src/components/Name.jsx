7723f5d93920ced619a17f3c293f193723bedf38
Alireza <andromedaa5692@gmail.com>
2026-09-17 14:58:51 +0330

    learn Object.is() shallow compare in memo

--- practice-learning/memorization-optimization-principles/memo/src/components/Name.jsx
+++ practice-learning/memorization-optimization-principles/memo/src/components/Name.jsx
@@ -1,10 +1,27 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 
 const Name = ({ name, setName }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   console.log(["[Name.jsx] - reRendered"]);
 
   return (
-    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={theme === "light" ? "light-mode" : "dark-mode"}
+      />
+      <div onClick={toggleTheme}>
+        <span style={{ cursor: "pointer" }}>تغییر تم</span>
+      </div>
+    </>
   );
 };
 
